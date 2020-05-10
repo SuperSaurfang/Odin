f38e950ca7839704583eddf53dea50f0f222838c
Stephan F <radiergummi90@gmx.de>
2020-05-10 12:18:33 +0200

    Install Angular Material

--- Sif/src/app/shared/shared.module.ts
+++ Sif/src/app/shared/shared.module.ts
@@ -4,8 +4,14 @@ import { ReactiveFormsModule} from '@angular/forms';
 
 import { NewlinePipe } from 'src/app/core/pipes'
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu'
+
 import { SideBarModule } from '../shared-modules/side-bar/side-bar.module';
 import { ApplicationBarModule} from '../shared-modules/application-bar/application-bar.module';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -18,7 +24,12 @@ import { ApplicationBarModule} from '../shared-modules/application-bar/applicati
     NewlinePipe,
     ReactiveFormsModule,
     SideBarModule,
-    ApplicationBarModule
+    ApplicationBarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatMenuModule,
+    MatSortModule
   ]
 })
 export class SharedModule { }
