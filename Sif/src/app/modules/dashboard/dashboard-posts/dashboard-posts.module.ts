b869e4a68c5bcd5e7a67121d4fdd89489dd7d1ad
Stephan F <radiergummi90@gmx.de>
2020-06-13 13:38:54 +0200

    improve dashboard module

--- Sif/src/app/modules/dashboard/dashboard-posts/dashboard-posts.module.ts
+++ Sif/src/app/modules/dashboard/dashboard-posts/dashboard-posts.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
 import { DashboardPostsComponent } from './dashboard-posts.component';
 import { DashboardPostRoutes } from './dashboard-post.routing';
 import { DashboardPostListComponent } from './dashboard-post-list/dashboard-post-list.component';
 import { DashboardPostEditorComponent } from './dashboard-post-editor/dashboard-post-editor.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     CKEditorModule,
-    DashboardPostRoutes
+    DashboardPostRoutes,
+    FormsModule,
+    SharedModule,
+    FontAwesomeModule
   ],
   declarations: [
     DashboardPostsComponent,
     DashboardPostListComponent,
-    DashboardPostEditorComponent
+    DashboardPostEditorComponent,
   ]
 })
 export class DashboardPostsModule {
