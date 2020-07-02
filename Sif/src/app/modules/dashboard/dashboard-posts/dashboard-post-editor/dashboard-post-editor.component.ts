dae527651fe83c0de71620a1969b14415ba8c27d
Stephan F <radiergummi90@gmx.de>
2020-07-02 14:20:18 +0200

    begin to implement autosave function for blog article

--- Sif/src/app/modules/dashboard/dashboard-posts/dashboard-post-editor/dashboard-post-editor.component.ts
+++ Sif/src/app/modules/dashboard/dashboard-posts/dashboard-post-editor/dashboard-post-editor.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { faAngleDown, faAngleUp, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ChangeEvent, CKEditorComponent } from '@ckeditor/ckeditor5-angular';
 
-import { RestService } from 'src/app/core/services';
-import { Article } from 'src/app/core';
+import { RestService, UserService } from 'src/app/core/services';
+import { Article, EChangeResponse } from 'src/app/core';
 
 @Component({
   selector: 'app-dashboard-post-editor',
@@ -18,20 +18,24 @@ export class DashboardPostEditorComponent implements OnInit {
   public editor = ClassicEditor;
   public isEdit = false;
 
-  public iconStatus = faAngleDown
-  public isStatusSettingOpen = false;
+  public iconSpinner = faSpinner;
+  public isSaving = false;
+  public isSaved = false;
 
   @ViewChild('blogEditor')
   public blogEditor: CKEditorComponent;
 
-  constructor(private route: ActivatedRoute, private restService: RestService) 
-  { 
+  constructor(private route: ActivatedRoute, 
+    private restService: RestService, 
+    private userService: UserService) 
+  {
     this.article = {
       creationDate: new Date(),
       modificationDate: new Date(),
       status: 'draft',
       hasCommentsEnabled: true,
-      hasDateAuthorEnabled: true
+      hasDateAuthorEnabled: true,
+      userId: this.userService.CurrentUserValue().userId
     }
   }
 
@@ -47,27 +51,70 @@ export class DashboardPostEditorComponent implements OnInit {
     })
   }
 
-  saveArticle() {
+  private saveArticle() {
+    this.isSaving = true;
     console.log(this.article);
+    if(!this.isEdit && this.article.title) {
+      this.restService.createBlog(this.article).subscribe(response => {
+        switch (response.ChangeResponse) {
+          case EChangeResponse.Change:
+            this.restService.getBlogId(this.article.title).subscribe(response => {
+              this.article.articleId = response;
+              this.isEdit = true;
+              this.isSaving = false;
+              this.isSaved = true;
+            })
+          case EChangeResponse.Error:
+          case EChangeResponse.NoChange:
+          default:
+            break;
+        }
+      })
+    } else {
+      this.updateArticle();
+    }
   }
 
-  onChange( {editor}: ChangeEvent) 
-  {
+  private updateArticle() {
+    this.restService.updateBlog(this.article).subscribe(response => {
+      console.log(response);
+      this.isSaving = false;
+      this.isSaved = true;
+    })
+  }
+
+  onChange( {editor}: ChangeEvent) {
     this.article.articleText = editor.getData();
   }
 
-  public openStatusSettings() {
-    this.isStatusSettingOpen = !this.isStatusSettingOpen;
-    if(this.isStatusSettingOpen) {
-      this.iconStatus = faAngleUp;
-    } else {
-      this.iconStatus = faAngleDown;
-    }
+  public onUpdateArticleTitle() {
+    this.saveArticle();
+  }
+
+  public onUpdateArticleText({editor}: ChangeEvent) {
+    this.article.articleText = editor.getData();
+    this.saveArticle();
+  }
+
+  public onStatusUpdate(event: string) {
+    this.article.status = event;
+    this.saveArticle();
+  }
+
+  onCommentAllowUpdate(event: boolean) {
+    this.article.hasCommentsEnabled = event;
+    this.saveArticle();
+  }
+
+  onDisplayAuthorDateUpdate(event: boolean) {
+    this.article.hasDateAuthorEnabled = event;
+    this.saveArticle();
   }
 
   public parseUpdatedDate(event: string){
     this.article.creationDate.setDate(new Date(event).getDate());
     this.article.creationDate.setMonth(new Date(event).getMonth());
     this.article.creationDate.setFullYear(new Date(event).getFullYear());
+    this.saveArticle();
   } 
 }
