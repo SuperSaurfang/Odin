be6c3b19081da8d350440a233cd0e5b5fa24633f
The Evil <Stephan.feige@yahoo.com>
2020-07-09 18:17:42 +0200

    imporove error handling for restservice and update global style rules

--- Sif/src/app/core/services/rest/rest.service.ts
+++ Sif/src/app/core/services/rest/rest.service.ts
@@ -2,8 +2,12 @@ import { Injectable} from '@angular/core';
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { Article, Comment, User, ChangeResponse } from '../../models';
+import { Article, Comment, User, ChangeResponse, EChangeResponse } from '../../models';
 import { environment } from '../../../../environments/environment';
+
+const ErrorResponse: ChangeResponse = {
+  ChangeResponse: EChangeResponse.Error
+};
 @Injectable()
 export class RestService {
 
@@ -12,7 +16,8 @@ export class RestService {
 
   public getBlog(): Observable<Article[]> {
     return this.httpClient.get<Article[]>(`${environment.restApi}/blog`).pipe(
-      map(article => this.parseDate(article))
+      map(article => this.parseDate(article)),
+      catchError(this.handleError<Article[]>('Failed to load blog', []))
     );
   }
 
@@ -21,41 +26,52 @@ export class RestService {
       map(article => {
         article.creationDate = new Date(article.creationDate);
         article.modificationDate = new Date(article.modificationDate);
-        return article
-      })
-    )
+        return article;
+      }),
+      catchError(this.handleError<Article>('Failed to load blog', new Article()))
+    );
   }
 
   public getFullBlog(): Observable<Article[]> {
     return this.httpClient.get<Article[]>(`${environment.restApi}/blog/admin`).pipe(
-      map(article => this.parseDate(article))
-    )
+      map(article => this.parseDate(article)),
+      catchError(this.handleError<Article[]>('Failed to load blog', []))
+    );
   }
 
-  public getArticleByTitle(title: string) : Observable<Article> {
+  public getArticleByTitle(title: string): Observable<Article> {
     return this.httpClient.get<Article>(`${environment.restApi}/blog/admin/${title}`).pipe(
       map(article => {
         article.creationDate = new Date(article.creationDate);
         article.modificationDate = new Date(article.modificationDate);
-        return article
-      })
-    )
+        return article;
+      }),
+      catchError(this.handleError<Article>('Failed to load blog', new Article()))
+    );
   }
 
-  public deleteArticles() : Observable<ChangeResponse> {
-    return this.httpClient.delete<ChangeResponse>(`${environment.restApi}/blog/admin`)
+  public deleteArticles(): Observable<ChangeResponse> {
+    return this.httpClient.delete<ChangeResponse>(`${environment.restApi}/blog/admin`).pipe(
+      catchError(this.handleError<ChangeResponse>('Failed to load blog', ErrorResponse))
+    );
   }
 
   public updateBlog(article: Article): Observable<ChangeResponse> {
-    return this.httpClient.put<ChangeResponse>(`${environment.restApi}/blog/admin`, article)
+    return this.httpClient.put<ChangeResponse>(`${environment.restApi}/blog/admin`, article).pipe(
+      catchError(this.handleError<ChangeResponse>('Failed to load blog', ErrorResponse))
+    );
   }
 
   public getBlogId(title: string): Observable<number> {
-    return this.httpClient.get<number>(`${environment.restApi}/blog/admin/id/${title}`);
+    return this.httpClient.get<number>(`${environment.restApi}/blog/admin/id/${title}`).pipe(
+      catchError(this.handleError<number>('Failed to load blog', -1))
+    );
   }
 
   public createBlog(article: Article): Observable<ChangeResponse> {
-    return this.httpClient.post<ChangeResponse>(`${environment.restApi}/blog/admin`, article);
+    return this.httpClient.post<ChangeResponse>(`${environment.restApi}/blog/admin`, article).pipe(
+      catchError(this.handleError<ChangeResponse>('Failed to load blog', ErrorResponse))
+    );
   }
 
   public getSite(title: string): Observable<Article[]> {
@@ -73,7 +89,7 @@ export class RestService {
 
   public postLogin(user: User): Observable<User> {
     return this.httpClient.post<User>(`${environment.restApi}/auth`, user).pipe
-    ( 
+    (
       catchError(this.handleError<User>('Login failed', new User()))
     );
   }
@@ -82,8 +98,8 @@ export class RestService {
       articles.forEach(article => {
         article.creationDate = new Date(article.creationDate);
         article.modificationDate = new Date(article.modificationDate);
-      })
-      return articles
+      });
+      return articles;
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
@@ -93,6 +109,6 @@ export class RestService {
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
-    }
+    };
   }
 }
