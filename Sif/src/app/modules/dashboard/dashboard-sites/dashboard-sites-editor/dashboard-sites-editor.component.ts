c997b22d568aa0d33d45851b5cb41a838c02bb00
The Evil <Stephan.feige@yahoo.com>
2020-07-21 20:18:29 +0200

    create sites editore component

---
+++ Sif/src/app/modules/dashboard/dashboard-sites/dashboard-sites-editor/dashboard-sites-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sites-editor',
+  templateUrl: './dashboard-sites-editor.component.html',
+  styleUrls: ['./dashboard-sites-editor.component.scss']
+})
+export class DashboardSitesEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
