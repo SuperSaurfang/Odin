52e5ecd5bc2cfca3681daa5dc39ab3cbac6b4826
The Evil <Stephan.feige@yahoo.com>
2020-07-21 19:59:13 +0200

    create module for site list and site editor

--- Sif/src/app/modules/dashboard/dashboard.routing.ts
+++ Sif/src/app/modules/dashboard/dashboard.routing.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
     path: 'dashboard', component: DashboardComponent, children: [
       { path: '', redirectTo: 'overview', pathMatch: 'full' },
       { path: 'overview', component: DashboardOverviewComponent },
-      { path: 'posts', loadChildren: () => import('./dashboard-posts/dashboard-posts.module').then(m => m.DashboardPostsModule) }
+      { path: 'posts', loadChildren: () => import('./dashboard-posts/dashboard-posts.module').then(m => m.DashboardPostsModule) },
+      { path: 'sites', loadChildren: () => import('./dashboard-sites/dashboard-sites.module').then(m => m.DashboardSitesModule) }
     ]
   },
 ];
