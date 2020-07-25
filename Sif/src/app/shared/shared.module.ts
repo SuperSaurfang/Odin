b8f4fd9180271386a9799099e2bb85620a705977
The Evil <Stephan.feige@yahoo.com>
2020-07-25 21:27:09 +0200

    rework popup menu to make the component reuseable

--- Sif/src/app/shared/shared.module.ts
+++ Sif/src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule} from '@angular/forms';
 
-import { NewlinePipe } from 'src/app/core/pipes'
+import { NewlinePipe } from 'src/app/core/pipes';
 
 import { SideBarModule } from '../shared-modules/side-bar/side-bar.module';
 import { ApplicationBarModule} from '../shared-modules/application-bar/application-bar.module';
 import { CheckboxModule } from '../shared-modules/checkbox/checkbox.module';
 import { ToggleSwitchModule } from '../shared-modules/toggle-switch/toggle-switch.module';
+import { MenuModule } from '../shared-modules/menu/menu.module';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { ToggleSwitchModule } from '../shared-modules/toggle-switch/toggle-switc
     SideBarModule,
     ApplicationBarModule,
     CheckboxModule,
-    ToggleSwitchModule
+    ToggleSwitchModule,
+    MenuModule
   ]
 })
 export class SharedModule { }
