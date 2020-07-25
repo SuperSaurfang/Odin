6359c8a537057409d413f9c0c494aa78467d6a8b
The Evil <Stephan.feige@yahoo.com>
2020-07-25 21:30:49 +0200

    implement base stuff for page list component

--- Sif/src/app/modules/dashboard/dashboard-pages/dashboard-pages.module.ts
+++ Sif/src/app/modules/dashboard/dashboard-pages/dashboard-pages.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ListActionBarModule } from '../shared-dashboard-modules/list-action-bar/list-action-bar.module';
 import { ArticleSettingModule } from '../shared-dashboard-modules/article-setting/article-setting.module';
+import { SharedModule } from '../../../shared/shared.module';
 
 import { DashboardPagesRoutes } from './dashboard-pages.routing';
 import { DashboardPagesComponent } from './dashboard-pages.component';
 import { DashboardPagesListComponent } from './dashboard-pages-list/dashboard-pages-list.component';
 import { DashboardPagesEditorComponent } from './dashboard-pages-editor/dashboard-pages-editor.component';
+import { RestPageService, ArticleFilterService } from '../services';
+import { StatusChangeMenuModule } from '../shared-dashboard-modules/status-change-menu/status-change-menu.module';
 
 
 @NgModule({
@@ -19,12 +22,19 @@ import { DashboardPagesEditorComponent } from './dashboard-pages-editor/dashboar
     FormsModule,
     FontAwesomeModule,
     ListActionBarModule,
-    ArticleSettingModule
+    ArticleSettingModule,
+    SharedModule,
+    StatusChangeMenuModule,
+    FontAwesomeModule
   ],
   declarations: [
     DashboardPagesComponent,
     DashboardPagesListComponent,
     DashboardPagesEditorComponent,
+  ],
+  providers: [
+    ArticleFilterService,
+    RestPageService
   ]
 })
 export class DashboardPagesModule { }
