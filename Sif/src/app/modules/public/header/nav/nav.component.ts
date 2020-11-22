e30573a255b4ced47d461106f46fa9e7732d4100
The Evil <Stephan.feige@yahoo.com>
2020-11-22 17:06:24 +0100

    fix nav component

--- Sif/src/app/modules/public/header/nav/nav.component.ts
+++ Sif/src/app/modules/public/header/nav/nav.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/core/services';
 import { NavMenu } from 'src/app/core';
 
-interface NavEntry {
-  routerlink: string | string[];
-  displayText: string;
-  isDropdown: boolean
-  childs?: NavEntry[];
-}
-
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -17,32 +10,15 @@ interface NavEntry {
 export class NavComponent implements OnInit {
 
   public isHover = false;
-  public navEntries: NavEntry[] = [{
-    routerlink: '/blog',
-    isDropdown: false,
-    displayText: 'Home',
-  }, {
-    routerlink: ['/site', 'Das bin ich'],
-    isDropdown: true,
-    displayText: 'Über mich',
-    childs: [{
-      displayText: 'Das mache ich!',
-      isDropdown: false,
-      routerlink: ['/site', 'Das mache ich']
-    }]
-  }, {
-    routerlink: ['/site', 'Meine Projekte'],
-    displayText: 'Meine Projekte',
-    isDropdown: false
-  }];
   public navMenu: NavMenu[] = [];
   constructor(private restService: RestService) {
-    this.restService.getNavMenu().subscribe(response => {
-      this.navMenu = response;
-    });
+
   }
 
   ngOnInit() {
+    this.restService.getNavMenu().subscribe(response => {
+      this.navMenu = response;
+    });
   }
 
   public onHover() {
