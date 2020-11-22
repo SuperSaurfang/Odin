4ca8241d0af28d8587d8cd44232982417dc6e181
The Evil <Stephan.feige@yahoo.com>
2020-11-22 19:45:24 +0100

    replace site module with page module

--- Sif/src/app/modules/public/public.routing.ts
+++ Sif/src/app/modules/public/public.routing.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', component: PublicComponent, children: [
     { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
     { path: 'blog/:title', loadChildren: () => import('./article/article.module').then(m => m.ArticleModule)},
-    { path: 'page/:title', loadChildren: () => import('./site/site.module').then(m => m.SiteModule) }
+    { path: 'page/:title', loadChildren: () => import('./page/Page.module').then(m => m.PageModule) }
   ]},
 ];
 
