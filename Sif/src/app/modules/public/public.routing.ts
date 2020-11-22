4c4dade6d60cae2309eb8b01d76e4088770fd518
The Evil <Stephan.feige@yahoo.com>
2020-11-22 20:01:53 +0100

    fix typo in public.routing.ts

--- Sif/src/app/modules/public/public.routing.ts
+++ Sif/src/app/modules/public/public.routing.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', component: PublicComponent, children: [
     { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
     { path: 'blog/:title', loadChildren: () => import('./article/article.module').then(m => m.ArticleModule)},
-    { path: 'page/:title', loadChildren: () => import('./page/Page.module').then(m => m.PageModule) }
+    { path: 'page/:title', loadChildren: () => import('./page/page.module').then(m => m.PageModule) }
   ]},
 ];
 
