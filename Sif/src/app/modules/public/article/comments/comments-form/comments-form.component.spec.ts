7153be5c6fd2c1e493551fb38b0313667e8388b0
The Evil <Stephan.feige@yahoo.com>
2021-01-09 15:02:23 +0100

    Update dependencies and and angular

--- Sif/src/app/modules/public/article/comments/comments-form/comments-form.component.spec.ts
+++ Sif/src/app/modules/public/article/comments/comments-form/comments-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CommentsFormComponent } from './comments-form.component';
 
@@ -6,7 +6,7 @@ describe('CommentsFormComponent', () => {
   let component: CommentsFormComponent;
   let fixture: ComponentFixture<CommentsFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CommentsFormComponent ]
     })
--- Sif/src/app/shared-modules/side-bar/side-bar-search/side-bar-search.component.spec.ts
+++ Sif/src/app/shared-modules/side-bar/side-bar-search/side-bar-search.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SideBarSearchComponent } from './side-bar-search.component';
 
@@ -6,7 +6,7 @@ describe('SideBarSearchComponent', () => {
   let component: SideBarSearchComponent;
   let fixture: ComponentFixture<SideBarSearchComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SideBarSearchComponent ]
     })
