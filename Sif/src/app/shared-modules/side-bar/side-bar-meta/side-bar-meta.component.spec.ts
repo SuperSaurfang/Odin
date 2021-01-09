d176280ce6ee08d4bddac1af50bd38be81481ccf
The Evil <Stephan.feige@yahoo.com>
2021-01-09 15:02:23 +0100

    Update dependencies and and angular

--- Sif/src/app/shared-modules/side-bar/side-bar-meta/side-bar-meta.component.spec.ts
+++ Sif/src/app/shared-modules/side-bar/side-bar-meta/side-bar-meta.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SideBarMetaComponent } from './side-bar-meta.component';
 
@@ -6,7 +6,7 @@ describe('SideBarMetaComponent', () => {
   let component: SideBarMetaComponent;
   let fixture: ComponentFixture<SideBarMetaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SideBarMetaComponent ]
     })
