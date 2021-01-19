76791d4bfa3398710526dae4d7d86ca0b39988f2
The Evil <Stephan.feige@yahoo.com>
2021-01-19 11:18:58 +0100

    refactor errorResponse in RestBase, update routes in ResPageService, update routes in RestPostsService

--- Sif/src/app/modules/dashboard/services/rest-posts/rest-posts.service.ts
+++ Sif/src/app/modules/dashboard/services/rest-posts/rest-posts.service.ts
@@ -2,56 +2,52 @@ import { Injectable } from '@angular/core';
 import { RestBase } from 'src/app/core/baseClass';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Article, ChangeResponse, ChangeResponseOld, StatusResponse } from 'src/app/core';
+import { Article, StatusResponse, StatusResponseType } from 'src/app/core';
 import { map, catchError } from 'rxjs/operators';
 
-const StatusError: StatusResponse = {
-  change: ChangeResponse.Error,
-  message: 'Http error'
-};
 
 @Injectable()
 export class RestPostsService extends RestBase {
 
   constructor(private httpClient: HttpClient) {
-    super();
+    super('adminblog');
   }
 
   public getFullBlog(): Observable<Article[]> {
-    return this.httpClient.get<Article[]>(`${this.basePath}/adminblog`).pipe(
+    return this.httpClient.get<Article[]>(`${this.basePath}`).pipe(
       map(article => this.parseDates(article)),
       catchError(this.handleError<Article[]>('Failed to load blog', []))
     );
   }
 
   public getBlogId(title: string): Observable<number> {
-    return this.httpClient.get<number>(`${this.basePath}/adminblog/id/${title}`).pipe(
+    return this.httpClient.get<number>(`${this.basePath}/id/${title}`).pipe(
       catchError(this.handleError<number>('Failed to load blog', -1))
     );
   }
 
   public getArticleByTitle(title: string): Observable<Article> {
-    return this.httpClient.get<Article>(`${this.basePath}/adminblog/${title}`).pipe(
+    return this.httpClient.get<Article>(`${this.basePath}/${title}`).pipe(
       map(article => this.parseDate(article)),
       catchError(this.handleError<Article>('Failed to load blog', new Article()))
     );
   }
 
   public createBlog(article: Article): Observable<StatusResponse> {
-    return this.httpClient.post<StatusResponse>(`${this.basePath}/adminblog`, article).pipe(
-      catchError(this.handleError<StatusResponse>('Failed to load blog', StatusError))
+    return this.httpClient.post<StatusResponse>(`${this.basePath}`, article).pipe(
+      catchError(this.handleError<StatusResponse>('Failed to load blog', this.errorResponse(StatusResponseType.Create)))
     );
   }
 
   public updateBlog(article: Article): Observable<StatusResponse> {
-    return this.httpClient.put<StatusResponse>(`${this.basePath}/adminblog`, article).pipe(
-      catchError(this.handleError<StatusResponse>('Failed to load blog', StatusError))
+    return this.httpClient.put<StatusResponse>(`${this.basePath}`, article).pipe(
+      catchError(this.handleError<StatusResponse>('Failed to load blog', this.errorResponse(StatusResponseType.Update)))
     );
   }
 
   public deleteArticles(): Observable<StatusResponse> {
-    return this.httpClient.delete<StatusResponse>(`${this.basePath}/adminblog`).pipe(
-      catchError(this.handleError<StatusResponse>('Failed to load blog', StatusError))
+    return this.httpClient.delete<StatusResponse>(`${this.basePath}`).pipe(
+      catchError(this.handleError<StatusResponse>('Failed to load blog', this.errorResponse(StatusResponseType.Delete)))
     );
   }
 
