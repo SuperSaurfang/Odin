a572d5cc19994771955e838a0f2f0a5985708d90
The Evil <Stephan.feige@yahoo.com>
2021-01-21 09:31:55 +0100

    implement nlog in thor

--- Thor/Program.cs
+++ Thor/Program.cs
@@ -1,26 +1,45 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace Thor
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+      try
+      {
+        logger.Info("Initialize Thor Rest API Application");
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception exception)
+      {
+        logger.Error(exception, "Unable to run application. Shutdown now.");
+      }
+      finally
+      {
+        LogManager.Shutdown();
+      }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host.CreateDefaultBuilder(args)
+          .ConfigureLogging(logging =>
+          {
+            logging.ClearProviders();
+            logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+          })
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+            webBuilder.UseStartup<Startup>();
+          })
+          .UseNLog();
+    }
+  }
 }
