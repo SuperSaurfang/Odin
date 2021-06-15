76f741360f870901915a645fbcccde54d3497a04
Stephan F <Stephan.feige@yahoo.com>
2021-06-15 10:41:35 +0200

    work on category editor and service

--- Sif/src/app/modules/dashboard/dashboard-posts/dashboard-post-category-editor/dashboard-post-category-editor.component.ts
+++ Sif/src/app/modules/dashboard/dashboard-posts/dashboard-post-category-editor/dashboard-post-category-editor.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faSave, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Category } from 'src/app/core';
 import { CategoryService } from '../../services/category/category.service';
 
+const DEFAULT_EDITING_INDEX = -1;
 @Component({
   selector: 'app-dashboard-post-category-editor',
   templateUrl: './dashboard-post-category-editor.component.html',
@@ -13,6 +14,8 @@ export class DashboardPostCategoryEditorComponent implements OnInit {
 
   public editIcon = faEdit;
   public trashIcon = faTrash;
+  public saveIcon = faSave;
+  public abortIcon = faTimes;
 
   public categoryList: Category[] = [];
 
@@ -21,6 +24,11 @@ export class DashboardPostCategoryEditorComponent implements OnInit {
     description: ['', Validators.required],
     parentId: ['']
   });
+
+  public currentEditing = DEFAULT_EDITING_INDEX;
+
+  // holds the restore item, that will be reassign if editing was abort by user
+  private restoreValue: string;
 
   constructor(private categoryService: CategoryService,
     private formBuilder: FormBuilder) { }
@@ -31,17 +39,74 @@ export class DashboardPostCategoryEditorComponent implements OnInit {
     });
   }
 
-  createCategory() {
-    console.log(this.createCategoryForm.value);
+  public createCategory() {
+    const formValue = this.createCategoryForm.value as Category;
+    this.createCategoryForm.reset();
+    this.categoryService.createCategory(formValue);
+  }
+
+  public deleteCategory(index: number) {
+    const item = this.categoryList[index];
+    if (item === undefined) {
+      return;
+    }
+
+    this.categoryService.deleteCategory(item.categoryId);
+    this.reset();
+  }
+
+  public updateCategory(index: number) {
+    const item = this.categoryList[index];
+    if (item === undefined) {
+      return;
+    }
+
+    this.categoryService.updateCategory(item);
+    this.reset();
   }
 
   public getParentNameById(id: number): string {
+    if (id === undefined) {
+      return;
+    }
+
+    // if the id was changed by the selection it will be a string?!
+    if (typeof id === 'string') {
+      // tslint:disable-next-line: radix
+      id = parseInt(id);
+    }
     const result = this.categoryList.find(item => item.categoryId === id);
     if (result === undefined) {
       return '';
     }
 
     return result.name;
+  }
+
+  public setCurrentEditing(index: number): void {
+    // prevent editing an other item before, the current wasn't saved
+    if (this.currentEditing !== DEFAULT_EDITING_INDEX) {
+      return;
+    }
+
+    // create a restore value to keep the data
+    this.restoreValue = JSON.stringify(this.categoryList[index]);
+    this.currentEditing = index;
+  }
+
+  public clearCurrentEditing(): void {
+    if (this.restoreValue === '' || this.currentEditing === DEFAULT_EDITING_INDEX) {
+      return;
+    }
+
+    // set the restore value to the current editing item, when the user don't want to save the changes
+    this.categoryList[this.currentEditing] = JSON.parse(this.restoreValue);
+    this.reset();
+  }
+
+  private reset(): void {
+    this.currentEditing = DEFAULT_EDITING_INDEX;
+    this.restoreValue = '';
   }
 
 }
--- Sif/src/app/modules/dashboard/services/category/category.service.ts
+++ Sif/src/app/modules/dashboard/services/category/category.service.ts
@@ -1,21 +1,97 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { Category } from 'src/app/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Category, ChangeResponse } from 'src/app/core';
 import { RestCategoryService } from '../rest-category/rest-category.service';
 
 @Injectable()
 export class CategoryService {
 
-
-  private categoryList = new Subject<Category[]>();
+  private categories: Category[] = [];
+  private categoryList = new BehaviorSubject<Category[]>(this.categories);
   constructor(private restService: RestCategoryService) { }
 
 
   public getCategoryList(): Observable<Category[]> {
     this.restService.getCategoryList().subscribe(response => {
+      this.categories = response;
       this.categoryList.next(response);
     });
     return this.categoryList;
+  }
+
+  public updateCategory(category: Category) {
+    console.log(category);
+    this.restService.updateCategory(category).subscribe(response => {
+      switch (response.change) {
+        case ChangeResponse.Change:
+          this.categories = this.update(this.categories, category);
+          this.next(this.categories);
+          break;
+        case ChangeResponse.NoChange:
+        case ChangeResponse.Error:
+        default:
+          break;
+      }
+    });
+  }
+
+  public deleteCategory(id: number) {
+    this.restService.deleteCategory(id).subscribe(response => {
+      switch (response.change) {
+        case ChangeResponse.Change:
+          this.categories = this.removeById(this.categories, id);
+          this.next(this.categories);
+          break;
+        case ChangeResponse.Error:
+        case ChangeResponse.NoChange:
+        default:
+          break;
+      }
+    });
+  }
+
+  public createCategory(category: Category) {
+    this.restService.createCategory(category).subscribe(response => {
+      switch (response.change) {
+        case ChangeResponse.Change:
+          this.categories = this.add(this.categories, category);
+          this.next(this.categories);
+          break;
+        case ChangeResponse.Error:
+        case ChangeResponse.NoChange:
+        default:
+          break;
+      }
+    });
+  }
+
+  private update(categories: Category[], category: Category): Category[] {
+    categories.map(item => {
+      if (item.categoryId === category.categoryId) {
+        item.articleCount = item.articleCount;
+        item.description = category.description;
+        item.name = category.name;
+        item.parentId = category.parentId;
+      }
+    });
+    return categories;
+  }
+
+  private removeById(categories: Category[], id: number): Category[] {
+    const index = categories.findIndex(item => item.categoryId === id);
+    if (index >= 0) {
+      categories.splice(index, 1);
+    }
+    return categories;
+  }
+
+  private add(categories: Category[], category: Category): Category[] {
+    categories.push(category);
+    return categories;
+  }
+
+  private next(categories: Category[]) {
+    this.categoryList.next(categories);
   }
 
 }
