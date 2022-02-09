d7a8097e65f5b966d5450f5eb17cc24879776cd0
Stephan F <Stephan.feige@yahoo.com>
2022-02-09 14:26:36 +0100

    Implement ControlValueAccessor for the checkbox

--- Sif/src/app/shared-modules/checkbox/checkbox.component.ts
+++ Sif/src/app/shared-modules/checkbox/checkbox.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, forwardRef } from '@angular/core';
 import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import { faSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss']
+  styleUrls: ['./checkbox.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckboxComponent implements OnInit {
-
-  constructor() { }
-
+export class CheckboxComponent implements OnInit, ControlValueAccessor {
   public checked = faCheckSquare;
   public unchecked = faSquare;
   public indeterminate = faMinusSquare;
@@ -22,15 +27,40 @@ export class CheckboxComponent implements OnInit {
   public isChecked = false;
 
   @Input()
-  public label = "";
+  public label = '';
 
   @Output()
-  public StateChanged = new EventEmitter<boolean>();
+  public change = new EventEmitter<boolean>();
+
+  controlValueAccessorChangeFn = (_: any) => { };
+  controlValueAccessorOnTouchedFn = (_: any) => { };
+
+  constructor() { }
 
   ngOnInit() {
   }
 
-  public stateChanged(event: MouseEvent) {
-    this.StateChanged.emit(event.target['checked']);
+  writeValue(value: any): void {
+    if (value) {
+      this.isChecked = value;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.controlValueAccessorChangeFn = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.controlValueAccessorOnTouchedFn = fn;
+  }
+
+  public onClick(event: MouseEvent) {
+    this.isChecked = !this.isChecked;
+    event.stopPropagation();
+    this.controlValueAccessorChangeFn(this.isChecked);
+    this.change.emit(this.isChecked);
+  }
+
+  public onChange(event: Event) {
+    event.stopPropagation();
   }
 }
