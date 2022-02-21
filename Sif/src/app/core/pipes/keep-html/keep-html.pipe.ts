afc0032bcd50c2f28ac967fbcaad85911627c90d
Stephan F <Stephan.feige@yahoo.com>
2022-02-21 16:51:31 +0100

    implement pipe for by passing ckeditor styles

---
+++ Sif/src/app/core/pipes/keep-html/keep-html.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'keepHtml'
+})
+export class KeepHtmlPipe implements PipeTransform {
+
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(value: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(value);
+  }
+
+}
