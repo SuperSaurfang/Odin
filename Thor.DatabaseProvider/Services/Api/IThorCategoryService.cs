3c2a0f7f23e6ec8c6cb6d1024ce1efda78cf5088
Stephan F <Stephan.feige@yahoo.com>
2022-04-14 10:05:49 +0200

    Refactor CategoryService

--- Thor.DatabaseProvider/Services/Api/IThorCategoryService.cs
+++ Thor.DatabaseProvider/Services/Api/IThorCategoryService.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Thor.Models.Dto;
+using Thor.Models.Dto.Responses;
 
 namespace Thor.DatabaseProvider.Services.Api;
 
 public interface IThorCategoryService
 {
   Task<IEnumerable<Category>> GetCategories();
-  Task<Category> CreateCategory(Category category);
-  Task UpdateCategory(Category category);
-  Task DeleteCategory(int id);
+  Task<StatusResponse<Category>> CreateCategory(Category category);
+  Task<StatusResponse<Category>> UpdateCategory(Category category);
+  Task<StatusResponse<IEnumerable<Category>>> DeleteCategory(int id);
 }
--- Thor.DatabaseProvider/Services/Implementations/DefaultCategoryService.cs
+++ Thor.DatabaseProvider/Services/Implementations/DefaultCategoryService.cs
@@ -7,31 +7,63 @@ using Thor.DatabaseProvider.Services.Api;
 using DTO = Thor.Models.Dto;
 using DB = Thor.Models.Database;
 using Thor.DatabaseProvider.Util;
+using Thor.Models.Dto.Responses;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Thor.DatabaseProvider.Services.Implementations;
 
 internal class DefaultCategoryService : IThorCategoryService
 {
   private readonly ThorContext context;
+  private readonly ILogger<DefaultCategoryService> logger;
 
-  public DefaultCategoryService(ThorContext context)
+  public DefaultCategoryService(ThorContext context, ILogger<DefaultCategoryService> logger)
   {
     this.context = context;
+    this.logger = logger;
   }
 
-  public async Task<DTO.Category> CreateCategory(DTO.Category category)
+  public async Task<StatusResponse<DTO.Category>> CreateCategory(DTO.Category category)
   {
-    var dbCategory = new DB.Category(category);
-    var result = await context.Categories.AddAsync(dbCategory);
-    await context.SaveChangesAsync();
-    return new DTO.Category(result.Entity);
+    var response = new StatusResponse<DTO.Category>() {
+      ResponseType = StatusResponseType.Create
+    };
+    try
+    {
+      var dbCategory = new DB.Category(category);
+      var tracking = await context.Categories.AddAsync(dbCategory);
+      await context.SaveChangesAsync();
+      response.Model = new DTO.Category(tracking.Entity);
+      response.Change = Change.Change;
+    }
+    catch (Exception ex)
+    {
+      logger.LogError("Error on creating new category", ex);
+      response.Change = Change.Error;
+    }
+    return response;
   }
 
-  public async Task DeleteCategory(int id)
+  public async Task<StatusResponse<IEnumerable<DTO.Category>>> DeleteCategory(int id)
   {
-    var entity = await context.Categories.Where(c => c.CategoryId == id).FirstOrDefaultAsync();
-    context.Categories.Remove(entity);
-    await context.SaveChangesAsync();
+    var response = new StatusResponse<IEnumerable<DTO.Category>>() {
+      ResponseType = StatusResponseType.Delete
+    };
+    try
+    {
+      var entity = await context.Categories.Where(c => c.CategoryId == id).FirstOrDefaultAsync();
+      context.Categories.Remove(entity);
+      await context.SaveChangesAsync();
+      response.Model = await GetCategories();
+      response.Change = Change.Change;
+    }
+    catch (Exception ex)
+    {
+      logger.LogError("Error on deleting category", ex);
+      response.Change = Change.Error;
+    }
+    return response;
   }
 
   public async Task<IEnumerable<DTO.Category>> GetCategories()
@@ -40,10 +72,24 @@ internal class DefaultCategoryService : IThorCategoryService
     return Utils.ConvertToDto<DB.Category, DTO.Category>(categories, category => new DTO.Category(category));
   }
 
-  public async Task UpdateCategory(DTO.Category category)
+  public async Task<StatusResponse<DTO.Category>> UpdateCategory(DTO.Category category)
   {
-    var dbCategory = new DB.Category(category);
-    context.Categories.Update(dbCategory);
-    await context.SaveChangesAsync();
+    var response = new StatusResponse<DTO.Category>() {
+      ResponseType = StatusResponseType.Update
+    };
+    try
+    {
+      var dbCategory = new DB.Category(category);
+      var tracking = context.Categories.Update(dbCategory);
+      await context.SaveChangesAsync();
+      response.Model = new DTO.Category(tracking.Entity);
+      response.Change = Change.Change;
+    }
+    catch (Exception ex)
+    {
+      logger.LogError("Error on updating category", ex);
+      response.Change = Change.Error;
+    }
+    return response;
   }
 }
